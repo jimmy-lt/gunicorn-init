bdcc70f70f5ab0cbe6012181c70325f89c8e4c6e
Jimmy Thrasibule <spack.world@gmail.com>
2011-04-01 14:37:00 +0200

    First commit.

---
+++ tree/etc/gunicorn/py/example.py
@@ -0,0 +1,13 @@
+# Example taken form the http://gunicorn.org/configure.html
+# page.
+
+import os
+
+def numCPUs():
+  if not hasattr(os, "sysconf"):
+    raise RuntimeError("No sysconf detected.")
+  return os.sysconf("SC_NPROCESSORS_ONLN")
+
+bind = "127.0.0.1:8000"
+workers = numCPUs() * 2 + 1
+
